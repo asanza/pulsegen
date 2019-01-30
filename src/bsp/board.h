913ac3897770c155ed55791fc24c2095d3b23612
Diego Asanza <f.asanza@gmail.com>
2019-01-30 19:43:03 +0100

    fix

---
+++ src/bsp/board.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019 Diego Asanza
+ * 
+ * SPDX-License-Identifier: Apache License 2.0
+ * 
+ * Created Date: Wednesday January 30th 2019
+ * Author: diego
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
