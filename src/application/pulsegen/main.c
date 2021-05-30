a2322a67762ca8e169f62766f77a365a63f479cf
Diego Asanza <f.asanza@gmail.com>
2021-05-30 15:54:50 +0200

    run simple main thread
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- src/application/pulsegen/main.c
+++ src/application/pulsegen/main.c
@@ -1,5 +1,28 @@
+#include <FreeRTOS.h>
+#include <task.h>
+
+void test(void* arg)
+{
+    while(1);
+}
 
 void main(void)
 {
+    xTaskCreate(test, "test", 124, NULL, 3, NULL);
+    vTaskStartScheduler();
     while(1);
+}
+
+void vApplicationStackOverflowHook( xTaskHandle *pxTask, signed char *pcTaskName ) {
+    asm ("BKPT");
+    while(1) ;
+}
+
+void vApplicationIdleHook( void ) {
+
+}
+
+void vApplicationMallocFailedHook( void ) {
+    asm ("BKPT");
+    while(1) ;
 }
